e496e46c608ed8c07824a91a688ab6ca795d894f
Petras Dominykas Tadaras <dtadaras@gmail.com>
2019-06-13 19:22:31 +0300

    sort menu added_needs corecctions

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function sort_menu_opening_function() {
+    sort_menu_activation_button.classList.toggle("sort_menu_activation_button");
+    sort_menu_activation_button.classList.toggle("sort_menu_activation_button_opened");
+    sort_menu_area.classList.toggle("sort_menu_area_closed");
+    sort_menu_area.classList.toggle("sort_menu_area_opened");
+    sort_menu_area_separation_line_1.classList.toggle("sort_menu_area_separation_lines_invisible");
+    sort_menu_area_separation_line_1.classList.toggle("sort_menu_area_separation_lines_visible");
+    
+}
+
