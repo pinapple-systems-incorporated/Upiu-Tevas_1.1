7cac3dc4e657c06ea66960909747eccae0da861a
Petras Dominykas Tadaras <dtadaras@gmail.com>
2019-06-14 17:26:18 +0300

    sort menu additions_needs work

--- js/functions.js
+++ js/functions.js
@@ -7,6 +7,14 @@ function sort_menu_opening_function() {
     sort_menu_area.classList.toggle("sort_menu_area_opened");
     sort_menu_area_separation_line_1.classList.toggle("sort_menu_area_separation_lines_invisible");
     sort_menu_area_separation_line_1.classList.toggle("sort_menu_area_separation_lines_visible");
-    
 }
 
+function sleepover_menu_opening_function() {
+    sleepover_sorting_parameters_activation_button.classList.toggle("sleepover_sorting_parameters_activation_button");
+    sleepover_sorting_parameters_activation_button.classList.toggle("sleepover_sorting_parameters_activation_button_opened");
+    sleepover_sorting_parameters_area.classList.toggle("sleepover_sorting_parameters_area_hidden");
+    sleepover_sorting_parameters_area.classList.toggle("sleepover_sorting_parameters_area_active");
+    sort_menu_area_separation_line_2.classList.toggle("sort_menu_area_separation_lines_invisible");
+    sort_menu_area_separation_line_2.classList.toggle("sort_menu_area_separation_lines_visible");
+
+}
