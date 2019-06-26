98400eabe533a17fd65a9dd5108be3c3e0bf60a9
Petras Dominykas Tadaras <dtadaras@gmail.com>
2019-06-26 12:48:38 +0300

    sort meniu finishing_needs styling

--- js/sort_menu_actions.js
+++ js/sort_menu_actions.js
@@ -19,4 +19,24 @@ function sleepover_menu_opening_function() {
 
 }
 
+function parking_menu_opening_function() {
+    parking_sorting_parameters_activation_button.classList.toggle("parking_sorting_parameters_activation_button");
+    parking_sorting_parameters_activation_button.classList.toggle("parking_sorting_parameters_activation_button_opened");
+    parking_sorting_parameters_area.classList.toggle("parking_sorting_parameters_area_hidden");
+    parking_sorting_parameters_area.classList.toggle("parking_sorting_parameters_area_active");
+    sort_menu_area_separation_line_3.classList.toggle("sort_menu_area_separation_lines_invisible");
+    sort_menu_area_separation_line_3.classList.toggle("sort_menu_area_separation_lines_visible");
+
+}
+
+function i_want_menu_opening_function() {
+    i_want_sorting_parameters_activation_button.classList.toggle("i_want_sorting_parameters_activation_button");
+    i_want_sorting_parameters_activation_button.classList.toggle("i_want_sorting_parameters_activation_button_opened");
+    i_want_sorting_parameters_area.classList.toggle("i_want_sorting_parameters_area_hidden");
+    i_want_sorting_parameters_area.classList.toggle("i_want_sorting_parameters_area_active");
+    sort_menu_area_separation_line_4.classList.toggle("sort_menu_area_separation_lines_invisible");
+    sort_menu_area_separation_line_4.classList.toggle("sort_menu_area_separation_lines_visible");
+
+}
+
 
